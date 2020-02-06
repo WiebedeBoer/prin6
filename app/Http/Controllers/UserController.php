36369654eb8180ea382807fa41d7d154c5d53281
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2020-02-06 11:45:38 +0100

    new user
    new user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    public function list()
+    {
+        
+        /*
+        $new_users = ['Johannes','Wiebe',];
+        return view('users', ['new_users'=>$new_users,]);
+        */
+
+        
+        //$new_users = \App\New_User::all();
+
+        $new_users = New_User::all();
+
+        dd($new_users);
+
+        return view('users', ['new_users' =>$new_users,]);
+        
+    }
+}
---
+++ app/New_User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class New_User extends Model
+{
+    //
+    //protected $table = 'new_users';
+    //protected $primaryKey = 'ID';
+    //public $incrementing = true;
+}
