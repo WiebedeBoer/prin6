60f203d6a592326e19f39f3305643e354f07dbd9
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2020-02-10 15:47:24 +0100

    bootstrap update
    bootstrap update

---
+++ database/migrations/2020_02_10_104928_create_servers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servers', function (Blueprint $table) {
+            $table->bigIncrements('server_id');
+            $table->string('server_name');
+            $table->string('server_type');
+            $table->string('server_otap');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servers');
+    }
+}
